746ad6d1e59a84d0993097c93ba76fddf73756cc
cristian <cristianmedinay@gmail.com>
2023-10-04 00:02:54 +0200

    cookies-sessions

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import './ui/assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import VueCookies from 'vue-cookies'
+import VueSession from 'vue-session'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +11,15 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueCookies,{
+    expires:'1d',
+    path:'/',
+    domain:'',
+    /* secure:false, */
 
+})
+app.use(VueSession)
+
+//$cookies set get remove iskey keys()
+//$session start set get id renew destroy
 app.mount('#app')
